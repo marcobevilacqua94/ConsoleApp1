1c56b5f820ee6bfa79e185adcf1c52975b0f30f3
fedem106 <federicomara.fm@gmail.com>
2024-06-26 18:17:57 +0200

    j

--- Program.cs
+++ Program.cs
@@ -68,149 +68,155 @@ class StartUsing
             email = "sherriburke@zillanet.com"
         };
 
-        //try
-        //{
 
-
-
-            //// Define a delegate that prints and returns the system tick count
-            //Func<object, int> action = (object ctx) =>
-            //{
-            //    int i = (int)obj;
-
-            //    // Make each thread sleep a different time in order to return a different tick count
-            //    Thread.Sleep(i * 100);
-
-            //    // The tasks that receive an argument between 2 and 5 throw exceptions
-            //    if (2 <= i && i <= 5)
-            //    {
-            //        throw new InvalidOperationException("SIMULATED EXCEPTION");
-            //    }
-
-            //    int tickCount = Environment.TickCount;
-            //    Console.WriteLine("Task={0}, i={1}, TickCount={2}, Thread={3}", Task.CurrentId, i, tickCount, Thread.CurrentThread.ManagedThreadId);
-
-            //    return tickCount;
-            //};
-
-            //// Construct started tasks
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    int index = i;
-            //    tasks.Add(Task<int>.Factory.StartNew(action, index));
-            //}
-
-            //try
-            //{
-            //    // Wait for all the tasks to finish.
-            //    Task.WaitAll(tasks.ToArray());
-
-            //    // We should never get to this point
-            //    Console.WriteLine("WaitAll() has not thrown exceptions. THIS WAS NOT EXPECTED.");
-            //}
-            //catch (AggregateException e)
-            //{
-            //    Console.WriteLine("\nThe following exceptions have been thrown by WaitAll(): (THIS WAS EXPECTED)");
-            //    for (int j = 0; j < e.InnerExceptions.Count; j++)
-            //    {
-            //        Console.WriteLine("\n-------------------------------------------------\n{0}", e.InnerExceptions[j].ToString());
-            //    }
-            //}
-
-
-            //     async void operate(AttemptContext ctx, int index)
-            //     {
-            //         for (int i = 0; i < chunkSize; i++)
-            //         {
-            //             var opt = await ctx.GetOptionalAsync(_collection, (index * chunkSize + i).ToString()).ConfigureAwait(false);
-            //             if (opt == null)
-            //                 await ctx.InsertAsync(_collection, (index * chunkSize + i).ToString(), documento).ConfigureAwait(false);
-            //             else
-            //                 await ctx.ReplaceAsync(opt, documento).ConfigureAwait(false);
-            //         }
-
-            //     };
-
-            //     var tasks = new List<Task>();
-
-
-            //     var watch = Stopwatch.StartNew();
-
-
-            //     var result = await _transactions.RunAsync(async (ctx) => 
-            //    {
-            //        //for (int i = 0; i < total / chunkSize; i++)
-            //        //{
-            //        //    tasks.Add(Task.Factory.StartNew(() => operate(ctx, i)));
-            //        //}
-            //        //Task.WaitAll(tasks.ToArray());
-
-            //        //await Parallel.ForEachAsync(Enumerable.Range(0, total/chunkSize), async (index, token) =>
-            //        //{
-            //        //    await operate(ctx, index).ConfigureAwait(false);
-            //        //    Console.Clear();
-            //        //    Console.Write($"Staged {(index + 1) * chunkSize} documents");
-            //        //}).ConfigureAwait(false);
-
-            //            await Parallel.ForEachAsync(Enumerable.Range(0, 10000), async (index, token) =>
-            //        {
-            //            var opt = await ctx.GetOptionalAsync(_collection, index.ToString()).ConfigureAwait(false);
-            //            if (opt == null)
-            //                await ctx.InsertAsync(_collection, index.ToString(), documento).ConfigureAwait(false);
-            //            else
-            //                await ctx.ReplaceAsync(opt, documento).ConfigureAwait(false);
-            //            Console.Write(index);
-            //            if (index % 100 == 0)
-            //            {
-            //                             Console.Clear();
-            //                             Console.Write($"Staged {index} documents");
-            //            }
-            //        }).ConfigureAwait(false);
-
-            //        await ctx.CommitAsync().ConfigureAwait(false);
-            //    }).ConfigureAwait(false);
-            //    watch.Stop();
-            //    var elapsedMs = watch.ElapsedMilliseconds;
-            //    Console.Clear();
-            //    Console.WriteLine(elapsedMs / 1000 + "s");
-            //}
-            //catch (TransactionCommitAmbiguousException e)
-            //{
-            //    Console.WriteLine("Transaction possibly committed");
-            //    Console.WriteLine(e);
-            //}
-            //catch (TransactionFailedException e)
-            //{
-            //    Console.WriteLine("Transaction did not reach commit point");
-            //    Console.WriteLine(e);
-            //}
-
-            //Transaction
-            var stopwatch = Stopwatch.StartNew();
-            try
+        async Task operate(AttemptContext ctx, int index)
+        {
+            for (int i = 0; i < chunkSize; i++)
             {
-                var result = await _transactions.RunAsync(async (ctx) =>
+                var opt = await ctx.GetOptionalAsync(_collection, (index * chunkSize + i).ToString()).ConfigureAwait(false);
+                if (opt == null)
+                    await ctx.InsertAsync(_collection, (index * chunkSize + i).ToString(), documento).ConfigureAwait(false);
+                else
+                    await ctx.ReplaceAsync(opt, documento).ConfigureAwait(false);
+            }
+
+        };
+
+
+
+        try
+        {
+       
+
+ 
+                 var watch = Stopwatch.StartNew();
+
+
+                 var result = await _transactions.RunAsync(async (ctx) => 
                 {
-                    for (int i = 0; i < 10_000; i++)
+
+
+                    await Parallel.ForEachAsync(Enumerable.Range(0, total/chunkSize), async (index, token) =>
                     {
-                        await ctx.InsertAsync(_collection, $"testDocument2{i}", documento).ConfigureAwait(false);
+                        await operate(ctx, index).ConfigureAwait(false);
                         Console.Clear();
-                        Console.Write($"Staged {i} documents. Time elapsed: {stopwatch.Elapsed.TotalSeconds}s");
-                    }
-                    Console.WriteLine($"Staging documents:{stopwatch.Elapsed.TotalSeconds}s, or {stopwatch.Elapsed.TotalMinutes}min");
-                    stopwatch.Restart();
+                        Console.Write($"Staged {(index + 1) * chunkSize} documents");
+                    }).ConfigureAwait(false);
+
+                        await Parallel.ForEachAsync(Enumerable.Range(0, 10000), async (index, token) =>
+                    {
+                        var opt = await ctx.GetOptionalAsync(_collection, index.ToString()).ConfigureAwait(false);
+                        if (opt == null)
+                            await ctx.InsertAsync(_collection, index.ToString(), documento).ConfigureAwait(false);
+                        else
+                            await ctx.ReplaceAsync(opt, documento).ConfigureAwait(false);
+                        Console.Write(index);
+                        if (index % 100 == 0)
+                        {
+                                         Console.Clear();
+                                         Console.Write($"Staged {index} documents");
+                        }
+                    }).ConfigureAwait(false);
+
                     await ctx.CommitAsync().ConfigureAwait(false);
                 }).ConfigureAwait(false);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                Console.Clear();
+                Console.WriteLine(elapsedMs / 1000 + "s");
             }
-            catch (Exception e)
+            catch (TransactionCommitAmbiguousException e)
             {
+                Console.WriteLine("Transaction possibly committed");
                 Console.WriteLine(e);
             }
-            stopwatch.Stop();
-            Console.WriteLine($"Committing documents:{stopwatch.Elapsed.TotalSeconds}s, or {stopwatch.Elapsed.TotalMinutes}min");
+            catch (TransactionFailedException e)
+            {
+                Console.WriteLine("Transaction did not reach commit point");
+                Console.WriteLine(e);
+            }
+
+            
 
         
     }
+
+    //     var tasks = new List<Task>();
+    //// Define a delegate that prints and returns the system tick count
+    //Func<object, int> action = (object ctx) =>
+    //{
+    //    int i = (int)obj;
+
+    //    // Make each thread sleep a different time in order to return a different tick count
+    //    Thread.Sleep(i * 100);
+
+    //    // The tasks that receive an argument between 2 and 5 throw exceptions
+    //    if (2 <= i && i <= 5)
+    //    {
+    //        throw new InvalidOperationException("SIMULATED EXCEPTION");
+    //    }
+
+    //    int tickCount = Environment.TickCount;
+    //    Console.WriteLine("Task={0}, i={1}, TickCount={2}, Thread={3}", Task.CurrentId, i, tickCount, Thread.CurrentThread.ManagedThreadId);
+
+    //    return tickCount;
+    //};
+
+    //// Construct started tasks
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    int index = i;
+    //    tasks.Add(Task<int>.Factory.StartNew(action, index));
+    //}
+
+    //try
+    //{
+    //    // Wait for all the tasks to finish.
+    //    Task.WaitAll(tasks.ToArray());
+
+    //    // We should never get to this point
+    //    Console.WriteLine("WaitAll() has not thrown exceptions. THIS WAS NOT EXPECTED.");
+    //}
+    //catch (AggregateException e)
+    //{
+    //    Console.WriteLine("\nThe following exceptions have been thrown by WaitAll(): (THIS WAS EXPECTED)");
+    //    for (int j = 0; j < e.InnerExceptions.Count; j++)
+    //    {
+    //        Console.WriteLine("\n-------------------------------------------------\n{0}", e.InnerExceptions[j].ToString());
+    //    }
+    //}
+
+
+    //for (int i = 0; i < total / chunkSize; i++)
+    //{
+    //    tasks.Add(Task.Factory.StartNew(() => operate(ctx, i)));
+    //}
+    //Task.WaitAll(tasks.ToArray());
+
+
+    //Transaction
+    //var stopwatch = Stopwatch.StartNew();
+    //try
+    //{
+    //    var result = await _transactions.RunAsync(async (ctx) =>
+    //    {
+    //        for (int i = 0; i < 10_000; i++)
+    //        {
+    //            await ctx.InsertAsync(_collection, $"testDocument2{i}", documento).ConfigureAwait(false);
+    //            Console.Clear();
+    //            Console.Write($"Staged {i} documents. Time elapsed: {stopwatch.Elapsed.TotalSeconds}s");
+    //        }
+    //        Console.WriteLine($"Staging documents:{stopwatch.Elapsed.TotalSeconds}s, or {stopwatch.Elapsed.TotalMinutes}min");
+    //        stopwatch.Restart();
+    //        await ctx.CommitAsync().ConfigureAwait(false);
+    //    }).ConfigureAwait(false);
+    //}
+    //catch (Exception e)
+    //{
+    //    Console.WriteLine(e);
+    //}
+    //stopwatch.Stop();
+    //Console.WriteLine($"Committing documents:{stopwatch.Elapsed.TotalSeconds}s, or {stopwatch.Elapsed.TotalMinutes}min");
 
     //public async Task BulkInsertInTxn()
     //{
